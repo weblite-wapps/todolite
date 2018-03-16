3ab51ad08e7d9be8ced1241dc48af7443a629cfa
ali <aliasgary73@gmail.com>
2018-03-16 20:41:18 +0330

    add new todolite vue boilerplate

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <TodoInput msg="1000-0-0-"/>
+  </div>
+</template>
+
+
+<script>
+import TodoInput from './components/TodoInput.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TodoInput,
+  }
+}
+</script>
+
+
+<style>
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'to',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
