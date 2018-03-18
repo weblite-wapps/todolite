9a4087ff66fe123e0c7397d9c79ac717b9cd00af
ali <aliasgary73@gmail.com>
2018-03-19 02:06:06 +0330

    add expanded

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,21 @@
 <template>
 <div :class="$style['todo-item']">
-  <Checkbox
-    @click="checked => onDone(id, checked)"
-    :default="!!functor"
-  />
+  <div :class="$style['first-row']">
+    <Checkbox
+      @click="checked => onDone(id, checked)"
+      :default="!!functor"
+    />
 
-  <div :class="$style['todo-item-title']">{{ title }}</div>
+    <div :class="$style['todo-item-title']">{{ title }}</div>
+
+    <i class="material-icons" @click="expande">
+      {{ expanded ? 'expand_less' : 'expand_more' }}
+    </i>
+  </div>
+
+  <div v-if="expanded">
+    {{ functor }}
+  </div>
 </div>
 </template>
 
@@ -18,7 +28,17 @@ export default {
 
   components: { Checkbox },
 
+  data: () => ({
+    expanded: false,
+  }),
+
   props: ['title', 'functor', 'id', 'onDone', 'onDelete', 'onEdit'],
+
+  methods: {
+    expande: function() {
+      this.expanded = !this.expanded
+    }
+  },
 }
 </script>
 
@@ -29,6 +49,9 @@ export default {
     border: 1px #E0E0E0 solid;
     margin-bottom: 10px;
     border-radius: 5px;
+  }
+
+  .first-row {
     display: flex;
     align-items: center;
   }
