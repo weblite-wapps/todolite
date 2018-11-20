7f7a70e475899fc769d5083b00832a4fdd80d887
ali <aliasgary73@gmail.com>
2018-11-20 17:57:29 +0330

    Add animation to controllers

--- src/components/TodoListItemHeaderControllers.vue
+++ src/components/TodoListItemHeaderControllers.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="controllers">
     <BaseToggle
+      class="toggle done"
       color="#60C102"
       icon="done"
       :value="done"
       @change="changeController('done', $event)"
     />
     <BaseToggle
+      class="toggle star"
       color="#FFAD00"
       icon="star"
       :value="star"
       @change="changeController('star', $event)"
     />
     <BaseToggle
+      class="toggle edit"
       color="#636363"
       icon="edit"
       :value="edit"
       @change="changeController('edit', $event)"
     />
     <BaseToggle
+      class="toggle remove"
       color="#DA4445"
       icon="remove"
       :value="remove"
@@ -60,5 +64,37 @@ export default {
 
 .controllerButton {
   flex: 1;
+}
+
+.toggle {
+  margin-top: -40px;
+}
+
+.done {
+  animation: slide 0.1s ease-in forwards;
+}
+
+.star {
+  margin-top: -40px;
+  animation: slide 0.1s ease-in 0.07s forwards;
+}
+
+.edit {
+  margin-top: -40px;
+  animation: slide 0.1s ease-in 0.14s forwards;
+}
+
+.remove {
+  margin-top: -40px;
+  animation: slide 0.1s ease-in 0.21s forwards;
+}
+
+@keyframes slide {
+  from {
+    margin-top: -40px;
+  }
+  to {
+    margin-top: 0px;
+  }
 }
 </style>
