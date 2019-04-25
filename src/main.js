12d53048f27961781d552c938fa6c1b8a11bde1c
Mostafa Mohseni Kabir <mostafa_mohsenikabir@yahoo.com>
2019-04-25 12:53:14 +0430

    feat(master): Add vue performance

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // modules
 import Vue from 'vue'
+
 // global css style
 import './helper/style/reset.scss'
 import './helper/style/global.scss'
 // components
 import App from './App.vue'
+
+Vue.config.devtools = true
+Vue.config.performance = process.env.NODE_ENV !== 'production'
 
 new Vue({ render: h => h(App) }).$mount('#app')
