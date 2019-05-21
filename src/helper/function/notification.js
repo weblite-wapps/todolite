17d665efbd23a7b294ea90b6b9aeecf80e75cdaa
Mostafa Mohseni Kabir <mostafa_mohsenikabir@yahoo.com>
2019-05-21 11:07:57 +0430

    fix(master): Fix notif message length

--- src/helper/function/notification.js
+++ src/helper/function/notification.js
@@ -1,15 +1,24 @@
 // R, W
-const { R, W } = window
+const {
+  R,
+  W
+} = window
 
-export default (type, { id, text: newText }, { todos, username }) => {
+export default (type, {
+  id,
+  text: newText
+}, {
+  todos,
+  username
+}) => {
   const text = type === 'add' ? newText : R.find(R.propEq('id', id), todos).text
 
   const content =
-    type === 'add'
-      ? [`Todo was added by ${username}`, text]
-      : type === 'done'
-      ? [`Todo was marked as done by ${username}`, text]
-      : undefined
+    type === 'add' ?
+    [`Added by ${username}`, text] :
+    type === 'done' ?
+    [`Done by ${username}`, text] :
+    undefined
 
   if (content) W.sendNotificationToAll(...content)
 }
